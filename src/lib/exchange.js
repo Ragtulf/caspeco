6cb8f72593f2917bce02323a89382b5916142224
Ragna <ragnaulfars@gmail.com>
2020-10-12 14:43:43 +0200

    Fetch working but problem with having the button refetch

--- src/lib/exchange.js
+++ src/lib/exchange.js
@@ -6,22 +6,47 @@ export const Exchange = () => {
   const API_KEY = process.env.REACT_APP_API_KEY
   const RATE_URL = `http://data.fixer.io/api/latest?access_key=${API_KEY}&symbols=SEK`
 
+
+  // const FetchNewData = () => {
+  //   useEffect(() => {
+  //     fetch(RATE_URL)
+  //       .then(res => res.json())
+  //       .then(json => {
+  //         console.log('Exchange rate', json)
+  //         setRateInfo(json)
+  //       })
+  //   }, [])
+  // }
+
+  // FetchNewData()
+
   useEffect(() => {
-    fetch(RATE_URL)
+    const fetchNewData = () => {
+      fetch(RATE_URL)
       .then(res => res.json())
       .then(json => {
-        console.log(process.env.REACT_APP_API_KEY)
-        console.log(API_KEY)
         console.log('Exchange rate', json)
         setRateInfo(json)
       })
+    }  
+
+    fetchNewData()
   }, [])
+
+  // DATE AND TIME OF FETCH
+  const timestamp = rateInfo.timestamp
+  let dateObject = new Date(timestamp * 1000)
+  let printTime = dateObject.toLocaleString('en-GB')
+
 
   return (
     <div>
       <h3>SEK ➡️ EUR</h3>
       {rateInfo && <h4>{rateInfo.rates.SEK}</h4>}
-      {rateInfo && <h5>{rateInfo.timestamp}</h5>}
+      {printTime && <h5>{printTime}</h5>}
+      <button onClick={() => FetchNewData()}>
+        Update
+      </button>
     </div>
   )
 }
