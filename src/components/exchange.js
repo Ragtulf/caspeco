6c0f516c7cb819a3bd412a5ab94249b1df0e2ac7
Ragna <ragnaulfars@gmail.com>
2020-10-15 10:40:02 +0200

    Fixed format on date

--- src/components/exchange.js
+++ src/components/exchange.js
@@ -3,37 +3,35 @@ import styled from 'styled-components'
 
 export const Exchange = () => {
   const [rateInfo, setRateInfo] = useState('')
-
-  // const API_KEY = process.env.REACT_APP_API_KEY
-  // const RATE_URL = `http://data.fixer.io/api/latest?access_key=${API_KEY}&symbols=SEK`
+  const [date, setDate] = useState('')
 
   const RATE_URL = 'https://api.exchangeratesapi.io/latest?symbols=SEK'
 
-  /* SINCE I'M NOT ON A SUBSCRIPTION PLAN WITH FIXER.IO
-   I ONLY GET HOURLY UPDATES ON THE RATES */
   const fetchNewData = () => {
     fetch(RATE_URL)
       .then(res => res.json())
       .then(json => {
         console.log('Exchange rate', json)
         setRateInfo(json)
+        setDate(json.date)
       })
   }
 
   useEffect(() => fetchNewData(), [])
 
+  // Fix format on date
+  const newDateFormat = date => {
+    const [yy, mm, dd] = date.split(/-/g)
+    return `${dd}/${mm}/${yy}`
+  }
+  const printDate = (newDateFormat(date))
 
-  // DATE AND TIME OF FETCH
-  // const timestamp = rateInfo.timestamp
-  // let dateObject = new Date(timestamp * 1000)
-  // let printDate = dateObject.toLocaleDateString('en-GB')
-
-
+  
   return (
     <WidgetDiv>
       <Heading>SEK ➡️ EUR</Heading>
       {rateInfo && <RateText>{rateInfo.rates.SEK}</RateText>}
-      {rateInfo && <DateText>{rateInfo.date}</DateText>}
+      {printDate && <DateText>{printDate}</DateText>}
       <UpdateButton onClick={() => fetchNewData()}>
         Update
       </UpdateButton>
